654b843a68a4a05d1f4b6c9894713955c571ab39
amalrails <amal.rubyonrails@gmail.com>
2020-10-21 17:04:40 -0300

    Seed rushing data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rushing.count == 0
+  path = File.join(File.dirname(__FILE__), "./seeds/rushing.json")
+  records = JSON.parse(File.read(path))
+  records.each do |record|
+    Rushing.create!(player: record['Player'], team: record['Team'], pos: record['Pos'],
+                    att: record['Att'], att_by_g: record['Att/G'], yds: record['Yds'],
+                    avg: record['Avg'], yds_by_g: record['Yds/G'], td: record['TD'],
+                    lng: record['Lng'], first: record['1st'], first_percentage: record['1st%'],
+                    twenty_plus: record['20+'], forty_plus: record['40+'], fum: record['FUM'])
+  end
+  puts "Rushings are seeded"
+end
